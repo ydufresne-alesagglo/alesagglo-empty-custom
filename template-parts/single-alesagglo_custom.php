8c756ef89b501213a67f5403e8e04b916eb92611
ydufresne-alesagglo <yann.dufresne@alesagglo.fr>
2025-09-09 15:50:12 +0200

    fix class name on single template

--- template-parts/single-alesagglo_custom.php
+++ template-parts/single-alesagglo_custom.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
-	<main id="publi-<?php the_ID(); ?>" <?php post_class(); ?> role="main">
-		<header class="publi-header">
+	<main id="custom-<?php the_ID(); ?>" <?php post_class(); ?> role="main">
+		<header class="custom-header">
 			<?php if (has_post_thumbnail()) {
 				the_post_thumbnail('', array('aria-hidden' => 'true'));
 			} ?>
-			<h1 class="publi-title" aria-label="<?php echo esc_attr( get_the_title() );?>">
+			<h1 class="custom-title" aria-label="<?php echo esc_attr( get_the_title() );?>">
 				<?php the_title(); ?>
 			</h1>
 		</header>
-		<div class="publi-content">
+		<div class="custom-content">
 			<?php the_excerpt(); ?>
 		</div>
 		<?php // add custom fields here
