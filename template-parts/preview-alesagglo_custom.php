f87e9c3adb118733538720be1f848596abebaa0e
ydufresne-alesagglo <yann.dufresne@alesagglo.fr>
2026-02-03 14:37:18 +0100

    wrapper for preview

--- template-parts/preview-alesagglo_custom.php
+++ template-parts/preview-alesagglo_custom.php
@@ -1,10 +1,12 @@
-		<?php if (has_post_thumbnail()) : ?>
-			<a href="<?php echo esc_url(get_permalink()); ?>" aria-label="lire l&rsquo;information compl&egrave;te">
-				<?php the_post_thumbnail('thumbnail', array('aria-hidden' => 'true')); ?>
-			</a>
-		<?php endif; ?>
-		<h2 class="preview-title" aria-label="<?php echo esc_attr( get_the_title() );?>">
-			<?php the_title( '<a href="' . esc_url(get_permalink()) . '" aria-label="lire l&rsquo;information compl&egrave;te">', '</a>' ); ?>
-		</h2>
-		<?php // add custom fields here
-		?>
+		<div class="list-item" role="listitem">
+			<?php if (has_post_thumbnail()) : ?>
+				<a href="<?php echo esc_url(get_permalink()); ?>" aria-label="lire l&rsquo;information compl&egrave;te">
+					<?php the_post_thumbnail('thumbnail', array('aria-hidden' => 'true')); ?>
+				</a>
+			<?php endif; ?>
+			<h2 class="preview-title" aria-label="<?php echo esc_attr( get_the_title() );?>">
+				<?php the_title( '<a href="' . esc_url(get_permalink()) . '" aria-label="lire l&rsquo;information compl&egrave;te">', '</a>' ); ?>
+			</h2>
+			<?php // add custom fields here
+			?>
+		</div>
